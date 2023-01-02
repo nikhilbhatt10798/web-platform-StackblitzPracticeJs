8246c173aa609599f6e9205ed11c12804d475a91
nikhilbhatt10798 <83267123+nikhilbhatt10798@users.noreply.github.com>
2023-01-02 21:52:17 +0530

    2 files modified

---
+++ 26_array.js
@@ -0,0 +1,22 @@
+// // array Push
+
+// let fruits = ['apple', 'mango', 'grapes'];
+
+// console.log(fruits);
+// // push
+// fruits.push('banana');
+// console.log(fruits);
+// // pop
+// let popFruite = fruits.pop();
+// console.log(fruits);
+
+// // push and pop are faster than shift and unshift
+
+// primitive vs reference Type
+
+//refrence type
+
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+console.log('arr1', arr1);
+console.log('arr2', arr2);
