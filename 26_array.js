6157e3ccfb9dee4d6201abaecccf83aeb7c2e3f5
nikhilbhatt10798 <83267123+nikhilbhatt10798@users.noreply.github.com>
2023-01-03 19:45:50 +0530

    1 file modified

--- 26_array.js
+++ 26_array.js
@@ -24,7 +24,7 @@
 // // how to clone array : ===================
 // // 1: using slice(0) function
 // // 2: using [],concat(array1)
-// // 3: sprade operator [...array1] 
+// // 3: sprade operator [...array1]
 
 // let array1 = ['item1','item2']
 // console.log('array1',array1)
@@ -32,13 +32,29 @@
 // let array2 = array1.slice(0)
 // // clone array 2: using [],concat(array1)
 // let array2 = [].concat(array1);
-// // clone array 3: sprade operator [...array1] 
-// let array2 = [...array1,'item3'] 
+// // clone array 3: sprade operator [...array1]
+// let array2 = [...array1,'item3']
 // console.log('array2',array2)
 
-
-// // how to concatenate two array 
+// // how to concatenate two array
 
 // let array1 = ['item1','item2']
-// let array2 = [...array1,'item3'] 
+// let array2 = [...array1,'item3']
 // console.log('array2',array2)
+
+// iterate
+const fruits = ['apples', 'mangos', 'banana', 'pineapple'];
+
+// method for iteration
+// 1: for loop
+// 2: forEach
+// 3: for of loop**
+
+// function test(ele,i){
+//   console.log(`${i}:${ele}`)
+// }
+// fruits.forEach(test)
+
+for (let ele of fruits) {
+  console.log(ele);
+}
