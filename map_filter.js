0efb4e5738821f52adfcd93e8970d5c66dc40707
nikhilbhatt10798 <83267123+nikhilbhatt10798@users.noreply.github.com>
2023-01-05 19:40:42 +0530

    1 file modified

--- map_filter.js
+++ map_filter.js
@@ -1,5 +1,6 @@
 // filter
 // map
+// first class function
 // transform
 
 // const num = [1, 2, 3, 4, 5, 6, 7];
@@ -19,16 +20,33 @@
 // let greate5 = num.filter((ele)=> ele >= 5 )
 // console.log(greate5);
 
-const student = [
-  { firstName: 'akash', marks: 100 },
-  { firstName: 'nikhil', marks: 65 },
-  { firstName: 'prajesh', marks: 70 },
-  { firstName: 'shreyash', marks: 100 },
-];
+// const student = [
+//   { firstName: 'akash', marks: 100 },
+//   { firstName: 'nikhil', marks: 65 },
+//   { firstName: 'prajesh', marks: 70 },
+//   { firstName: 'shreyash', marks: 100 },
+// ];
 
-function condition(ele) {
-  return ele.marks < 70;
-}
+// function condition(ele) {
+//   return ele.marks < 70;
+// }
 
-const filterData = student.filter(condition);
-console.log(filterData);
+// const filterData = student.filter(condition);
+// console.log(filterData);
+
+// map () =====================
+
+// first class function are those who accept function and argument
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function test (el){
+//   return el * 2
+// }
+// const newMember = num.map(test);
+
+// console.log(newMember);
+
+const newMember = num.map((el) => el * 2);
+console.log(num);
+console.log(newMember);
